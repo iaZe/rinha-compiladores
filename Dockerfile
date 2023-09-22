fe1e64e65e696858c1b9015e3b3b74516077f3d7
Daniel Tavares <nieltvrs@gmail.com>
2023-09-22 17:29:46 -0300

    Luajit && Bigints

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@ FROM ubuntu:latest
 
 RUN apt-get update && apt-get install -y \
     lua5.1 \
+    luajit \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
 
 COPY . .
 
-ENTRYPOINT ["lua", "interpreter.lua", "source.rinha"]
+ENTRYPOINT ["luajit", "interpreter.lua", "source.rinha"]
 
---
+++ src/bigints.lua
@@ -0,0 +1,43 @@
+function fibonacci(n, a, b)
+    if n == 0 then
+        return a
+    else
+        return fibonacci(n - 1, b, addBigInts(a, b))
+    end
+end
+
+function addBigInts(a, b)
+    local maxLength = math.max(#a, #b)
+    local carry = 0
+    local result = {}
+
+    for i = 1, maxLength do
+        local digitA = a[i] or 0
+        local digitB = b[i] or 0
+        local sum = digitA + digitB + carry
+        carry = math.floor(sum / 10)
+        table.insert(result, sum % 10)
+    end
+
+    if carry > 0 then
+        table.insert(result, carry)
+    end
+
+    return result
+end
+
+function stringToBigInt(str)
+    local result = {}
+    for i = #str, 1, -1 do
+        table.insert(result, tonumber(str:sub(i, i)))
+    end
+    return result
+end
+
+function bigIntToString(bigInt)
+    local str = ""
+    for i = #bigInt, 1, -1 do
+        str = str .. bigInt[i]
+    end
+    return str
+end
